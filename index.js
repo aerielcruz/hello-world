f48fa0e8b460a88cc68170f72fc4909427064e67
Aeriel Matthew Cruz <aerielmatthew@gmail.com>
2025-07-26 16:52:12 +1200

    Clean up

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const express = require('express');
 const app = express();
 const { getParameterStore } = require('./util/get-parameter-store')
 
-app.use(async (req, res, next) => {
-  console.log('TEST')
-  next()
-})
-
 app.get('/', async function (req, res) {
   const MONGODB_URI = await getParameterStore("mongodb-uri")
   res.send(`Parameter store value: ${MONGODB_URI}`);
