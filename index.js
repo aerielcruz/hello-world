8b3b727164abd00f9b30347d0791206f77fa75bb
Aeriel Matthew Cruz <aerielmatthew@gmail.com>
2025-07-26 16:37:20 +1200

    Return parameter store value

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const app = express();
 const { getParameterStore } = require('./util/get-parameter-store')
 
 app.use(async (req, res, next) => {
-  // const MONGODB_URI = await getParameterStore("mongodb-uri")
-  // console.log('Parameter store value:', MONGODB_URI)
   console.log('TEST')
   next()
 })
 
 app.get('/', async function (req, res) {
   const MONGODB_URI = await getParameterStore("mongodb-uri")
-  console.log('Parameter store value:', MONGODB_URI)
-  res.send('Hello World!');
+  res.send('Parameter store value:', MONGODB_URI);
 });
 
 const PORT = process.env.PORT || 3000;
