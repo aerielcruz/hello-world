9e91a0b3f4e1a9a694ed157d75cf61f6de70687f
Aeriel Matthew Cruz <aerielmatthew@gmail.com>
2025-07-27 21:28:30 +1200

    Clean up

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const app = express();
-const { getParameterStore } = require('./util/get-parameter-store')
 const { getAppConfig } = require('./util/get-app-config')
 
 app.get('/', async function (req, res) {
-  // AWS
-  // const MONGODB_URI = await getParameterStore("mongodb-uri")
-
-  // Azure
   const MONGODB_URI = await getAppConfig("mongodb-uri")
-
-  res.send(`Parameter store value: ${MONGODB_URI}`);
+  res.send(`App config value: ${MONGODB_URI}`);
 });
 
 const PORT = process.env.PORT || 3000;
